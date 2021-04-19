60bf339fcba0d49abe9c742e438dc711defc8c28
Ali <avida.chatraee2001@yahoo.com>
2021-04-19 17:18:32 +0430

    fix E5

--- E5.js
+++ E5.js
@@ -1,19 +1,15 @@
 // * merge arrays with concat and apply
-function mergeArrays(array) {
-  return Array.prototype.concat.apply([], array);
-}
+// function mergeArrays(array) {
+//   return Array.prototype.concat.apply([], array);
+// }
 
 // * merge arrays with loop
-// function mergeArrays(array) {
-//   let output = [];
-//   for (let iterator of array) {
-//     Array.isArray(iterator) ? mergeArrays(iterator) : output.push(iterator);
-//   }
-//   return output;
-// }
+let output = [];
+function mergeArrays(array) {
+  for (let iterator of array) {
+    Array.isArray(iterator) ? mergeArrays(iterator) : output.push(iterator);
+  }
+  return output;
+}
 
-// * merge arrays with flat method
-// function mergeArrays(array) {
-//   return array.flat(Infinity);
-// }
 console.log(mergeArrays([12, [23, 231, 12], [1, 2221], 443]));
