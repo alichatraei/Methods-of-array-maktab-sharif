ceacdbdd5dd2fbff39b55d74465826a10bc61724
Ali <avida.chatraee2001@yahoo.com>
2021-04-19 17:31:17 +0430

    Edit E3

--- E3.js
+++ E3.js
@@ -1,18 +1,22 @@
-// Remove duplicate items in an array
+// Remove duplicate items in an array (oldway)
+// function removeDuplicateItems(array) {
+//   const result = [];
+//   for (let i = 0; i < array.length; i++) {
+//     let exists = false;
+//     for (j = 0; j < result.length; j++) {
+//       if (array[i] === result[j]) {
+//         exists = true;
+//         break;
+//       }
+//     }
+//     if (!exists) {
+//       result.push(array[i]);
+//     }
+//   }
+//   return result;
+// }
+// * use newest way
 function removeDuplicateItems(array) {
-  const result = [];
-  for (let i = 0; i < array.length; i++) {
-    let exists = false;
-    for (j = 0; j < result.length; j++) {
-      if (array[i] === result[j]) {
-        exists = true;
-        break;
-      }
-    }
-    if (!exists) {
-      result.push(array[i]);
-    }
-  }
-  return result;
+  return [...new Set(array)];
 }
 console.log(removeDuplicateItems([23, 4, 12, 4, 5]));
--- E4.js
+++ E4.js
@@ -1,5 +1,6 @@
 // Extract numbers from a string use Regex
 function extractNumbers(str) {
   return str.match(/\d+/g);
+  // return Number.parseFloat(str);
 }
 console.log(extractNumbers("I'm 20. My height is 185 cm"));
